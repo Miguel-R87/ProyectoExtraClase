e69607469aa79b23dfe6f10c7bcf2e6ec38838cc
andre <andre@La_bestia>
2025-10-27 21:30:58 -0500

    Correccion findById en UsuarioTarea

--- Demo/src/main/java/co/edu/co/extraclase/data/dao/entity/postgresql/TaskUserPostgreSqlDAO.java
+++ Demo/src/main/java/co/edu/co/extraclase/data/dao/entity/postgresql/TaskUserPostgreSqlDAO.java
@@ -3,12 +3,10 @@ package co.edu.co.extraclase.data.dao.entity.postgresql;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import org.apache.catalina.valves.ParameterLimitValve;
 
 import co.edu.co.extraclase.crosscuting.exception.ExtraClaseException;
 import co.edu.co.extraclase.crosscuting.helper.BooleanHelper;
@@ -219,71 +217,8 @@ public class TaskUserPostgreSqlDAO extends SqlConnection implements TaskUserDAO{
 
 	@Override
     public TaskUserEntity findById(UUID id) {
-        final var sql = new StringBuilder();
-        var taskUser = new TaskUserEntity();
-
-        sql.append("SELECT ");
-        sql.append("tu.usuarioTareaId, ");
-        sql.append("pu.usuarioProyectoId, ");
-        sql.append("p.proyectoId, ");
-        sql.append("p.nombre AS nombreProyecto, ");
-        sql.append("u.usuarioId, ");
-        sql.append("u.primerNombre AS nombreUsuario, ");
-        sql.append("t.tareaId, ");
-        sql.append("t.titulo AS nombreTarea, ");
-        sql.append("tu.fechaAsignacion, ");
-        sql.append("tu.fechaFinalizacion, ");
-        sql.append("tu.esCreador, ");
-        sql.append("tu.comentario ");
-        sql.append("FROM UsuarioTarea tu ");
-        sql.append("INNER JOIN UsuarioProyecto pu ON tu.usuarioProyecto = pu.usuarioProyectoId ");
-        sql.append("INNER JOIN Tarea t ON tu.tarea = t.tareaId ");
-        sql.append("INNER JOIN Proyecto p ON pu.proyecto = p.proyectoId ");
-        sql.append("INNER JOIN Usuario u ON pu.usuario = u.usuarioId ");
-        sql.append("WHERE tu.usuarioTareaId = ?;");
-
-        try (final PreparedStatement preparedStatement = this.getConnection().prepareStatement(sql.toString())) {
-            preparedStatement.setObject(1, id);
-
-            try (var resultSet = preparedStatement.executeQuery()) {
-                if (resultSet.next()) {
-                    var projectUser = new ProjectUserEntity();
-                    projectUser.setProjectUserId(UUIDHelper.getUUIDHelper().getFromString(resultSet.getString("usuarioProyectoId")));
-
-                    var project = new ProjectEntity();
-                    project.setProjectId(UUIDHelper.getUUIDHelper().getFromString(resultSet.getString("proyectoId")));
-                    project.setName(resultSet.getString("nombreProyecto"));
-
-                    var user = new UserEntity();
-                    user.setUserId(UUIDHelper.getUUIDHelper().getFromString(resultSet.getString("usuarioId")));
-                    user.setFirstName(resultSet.getString("nombreUsuario"));
-
-                    var task = new TaskEntity();
-                    task.setTaskId(UUIDHelper.getUUIDHelper().getFromString(resultSet.getString("tareaId")));
-                    task.setTitle(resultSet.getString("nombreTarea"));
-
-                    taskUser.setTaskUserId(UUIDHelper.getUUIDHelper().getFromString(resultSet.getString("usuarioTareaId")));
-                    taskUser.setProjectUser(projectUser);
-                    taskUser.setTask(task);
-                    taskUser.setAssignmentDate(resultSet.getTimestamp("fechaAsignacion").toLocalDateTime());
-                    taskUser.setCompletionDate(resultSet.getTimestamp("fechaFinalizacion").toLocalDateTime());
-                    taskUser.setCreator(resultSet.getBoolean("esCreador"));
-                    taskUser.setComment(resultSet.getString("comentario"));
-                }
-            }
-
-        } catch (final SQLException exception) {
-            var userMessage = "Se presentó un problema tratando de encontrar la información del UsuarioTarea. Intente de nuevo.";
-            var technicalMessage = "SQLException al buscar UsuarioTarea: " + exception.getMessage();
-            throw ExtraClaseException.create(exception, userMessage, technicalMessage);
-        } catch (final Exception exception) {
-            var userMessage = "Se presentó un problema inesperado tratando de encontrar la información del UsuarioTarea.";
-            var technicalMessage = "Excepción inesperada al buscar UsuarioTarea: " + exception.getMessage();
-            throw ExtraClaseException.create(exception, userMessage, technicalMessage);
-        }
-
-        return taskUser;
-    }
+		return findByFilter(new TaskUserEntity()).stream().findFirst().orElse(new TaskUserEntity());
+	}
 
 	@Override
 	public void update(TaskUserEntity entity) {
