58d5fc8ff35ee82895cead31b64bb4f18eb36a74
Miguel Rincon <miguelitorincon2015@gmail.com>
2025-11-04 01:31:55 -0500

    implementación de Response y usercontroller tener en cuenta que se cambio el puerto en application properties

---
+++ Demo/src/main/java/co/edu/co/extraclase/initializer/ExtraClaseApplication.java
@@ -0,0 +1,18 @@
+package co.edu.co.extraclase.initializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan(basePackages = {"co.edu.co.extraclase"})
+
+
+@SpringBootApplication
+public class ExtraClaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtraClaseApplication.class, args);
+	}
+}
+
+
+
+
